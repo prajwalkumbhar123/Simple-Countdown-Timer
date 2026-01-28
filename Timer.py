fc2e85e8b182251443eff4070d3f066d4b8c432a
Prajwal Kumbhar <prajwalkumbhar61@gmail.com>
2026-01-28 23:34:49 +0530

    Add files via upload

---
+++ Timer.py
@@ -0,0 +1,12 @@
+import time
+
+print("=== Countdown Timer ===")
+
+seconds = int(input("Enter time in seconds: "))
+
+while seconds > 0:
+    print("Time left:", seconds, "seconds")
+    time.sleep(1)
+    seconds -= 1
+
+print("⏰ Time's up!")
